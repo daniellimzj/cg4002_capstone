088468df0b123e924cdb6e6cef869ee8f0af050a
daniellimzj <daniellimzijie@gmail.com>
2022-09-04 17:07:44 +0800

    Hide xilinx password in config file

--- ext_comms/ssh_test.py
+++ ext_comms/ssh_test.py
@@ -5,24 +5,31 @@ from socket import *
 if __name__ == "__main__":
 
     config = json.load(open('config.json'))
-    password = bytes(config["sshPassword"], encoding="utf-8")
-
+    sunfirePassword = bytes(config["sunfirePassword"], encoding="utf-8")
+    xilinxPassword = bytes(config["xilinxPassword"], encoding="utf-8")
 
     with sshtunnel.open_tunnel(
-        'sunfire.comp.nus.edu.sg',
+        ssh_address_or_host=('sunfire.comp.nus.edu.sg', 22),
         ssh_username="danielim",
-        ssh_password=password,
-        local_bind_address=('localhost', 9696),
-        remote_bind_address=('192.168.95.234', 9696)
-    ) as tunnel:
+        ssh_password=sunfirePassword,
+        remote_bind_address=('192.168.95.234', 22),
+        local_bind_address=('localhost', 6969)
+    ) as tunnel1:
+        print('Connection to Sunfire from port ' + str(tunnel1.local_bind_port) + " OK...")
+        with sshtunnel.open_tunnel(
+            ssh_address_or_host=('localhost', tunnel1.local_bind_port),
+            remote_bind_address=('127.0.0.1', 9696),
+            local_bind_address=('127.0.0.1', 9696),
+            ssh_username='xilinx',
+            ssh_password=xilinxPassword,
+        ) as tunnel2:
 
-        serverName = 'localhost'
-        serverPort = 9696
-        clientSocket = socket(AF_INET, SOCK_STREAM)
-        clientSocket.connect((serverName, serverPort))
-        print("connected to:", serverName, serverPort)
-        message = input('Enter a message: ')
-        clientSocket.send(message.encode())
-        receivedMsg = clientSocket.recv(2048)
-        print("Received back:", receivedMsg)
-
+            serverName = 'localhost'
+            serverPort = 9696
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((serverName, serverPort))
+            print("connected to:", serverName, serverPort)
+            message = input('Enter a message: ')
+            clientSocket.send(message.encode())
+            receivedMsg = clientSocket.recv(2048)
+            print("Received back:", receivedMsg)
