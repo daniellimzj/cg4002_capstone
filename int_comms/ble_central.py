e9cb81ad54a22e289090ed4300b8dea88955fdd6
BryanElmer <bryan.elmer.mulijono@gmail.com>
2022-09-10 04:58:01 +0800

    Initial commit for central with bluepy

--- int_comms/ble_central.py
+++ int_comms/ble_central.py
@@ -1,136 +1,68 @@
-import asyncio
-import platform
-import sys
-import struct
+from bluepy.btle import Peripheral, DefaultDelegate
 
-from bleak import BleakClient, BleakScanner
-from bleak.exc import BleakError
+# TODO: Implement Handshake
+# TODO: Test handshake functionality
+# TODO: Standardise packets to 20 bytes
+# TODO: Handle packet fragmentation; wait for 20 bytes of data before processing
 
+TIMEOUT_HANDSHAKE = 50
 
-beetleIDs = [
-    "17AE54AA-415D-4B76-AD40-F90E21A7F342",
-    "17AE54AA-415D-4B76-AD40-F90E21A7F343"
+btleAddrs = [
+    "D0:39:72:BF:CA:CF",
+    "D0:39:72:BF:CA:CF",
+    "D0:39:72:BF:CA:CF"
 ]
 
-beetleHandshakes = [False] * 3
-
-# class BLEComms():
-#     def __init__(self, beetle, serialCharacteristic, index):
-#         self.beetle = beetle
-#         self.serialCharacteristic = serialCharacteristic
-#         self.index = index
-#
-#    def ackHandler(self):
-#        self.beetle.write_gatt_char(self.serialCharacteristic, bytes("A", "utf-8"))
-#        beetleHandshakes[self.index] = True
-# 
-#     def notificationHandler(self, sender, data):
-#         # TODO: Verify the checksum
-#         try:
-#             packetFormat = {
-#                 '<b' # Packet Type (1)
-#                 '7h' # Data (14)
-#                 'l' # Timestamp (4)
-#                 'b' # Checksum (1)
-#             }
-
-#             packet = struct.unpack(packetFormat, data)
-#             packetType = packet[0]
-
-#             if packetType == ord('A'):
-#                 self.ackHandler()
-
-#         except Exception as e:
-#             raise BleakError(e)
-
-# def initHandshake(beetle, serialCharacteristic, index):
-#     while not beetleHandshakes[index]:
-#         print("Sending hello to Beetle", str(index))
-#         beetle.write_gatt_char(serialCharacteristic, bytes("H", "utf-8"))
+btleHandshakes = [False] * 3
 
 
+class Comms(DefaultDelegate):
+    def __init__(self, serialChar, index):
+        DefaultDelegate.__init__(self)
+        self.serialChar = serialChar
+        self.index = index
 
-# async def beetleThread(idList, index):
-#     isFirstLoop = True
-#     serialService = None
-#     serialCharacteristic = None
+    def handleAckPacket(self):
+        self.serialChar.write(bytes("A", "utf-8"))  # TODO: Change to 20bytes
+        btleHandshakes[self.index] = True
 
-#     while True:
-#         try:
-#             print("Searching for Beetle", str(index))
-#             device = await BleakScanner.find_device_by_address(idList, timeout=5.0)
-            
-#             print("Connecting to Beetle...", str(index))
-#             async with BleakClient(device) as beetle:
-#                 print("Beetle connected: {beetle.is_connected}")
-#                 if isFirstLoop:
-#                     serialService = beetle.services.get_service("0000dfb0-0000-1000-8000-00805f9b34fb")
-#                     serialCharacteristic = serialService.get_characteristic("0000dfb1-0000-1000-8000-00805f9b34fb")
-#                     # print(serialCharacteristic)
-#                     comms = BLEComms(beetle, serialCharacteristic, index)
-#                     beetle.start_notify(serialCharacteristic, comms.notificationHandler)
+    def verifyChecksum(self, data):
+        pass
+
+    def handleNotification(self, charHandle, data):
+        pass
 
 
-#                 print("Initialising handshake for Beetle", str(index))
-#                 initHandshake(beetle, serialCharacteristic, index)
+def initHandshake(beetle, serialChar, index):
+    while not btleHandshakes[index]:
+        print("Sending Hello Packet")
+        serialChar.write(bytes("H", "utf-8"))
+
+        if beetle.waitForNotifications(TIMEOUT_HANDSHAKE):
+            pass
 
 
-#         except Exception as e:
-#             raise BleakError(f"A device with address {idList} could not be found.")
-
-def notificationHandler(sender, data):
-    print("{0}: {1}".format(sender, data))
-
-async def beetleThread(idList, index):
+def beetleThread(addr, index):  # Curr beetle addr, curr beetle index
     isFirstLoop = True
-    serialService = None
-    serialCharacteristic = None
+    serialSvc = None
+    serialChar = None
+    beetle = Peripheral()
 
-    # while True:
     try:
-            # print("Searching for Beetle", str(index))
-            # device = await BleakScanner.find_device_by_address(idList[index], timeout=10.0)
-            
-            print("Connecting to Beetle", str(index))
-            async with BleakClient(idList[index]) as beetle:
-                print("Beetle connected: {beetle.is_connected}")
+        print("Searching for Beetle", str(index))
+        beetle.connect(addr)
+        print("Connecting...")
 
-                if isFirstLoop:
-                    serialService = beetle.services.get_service("0000dfb0-0000-1000-8000-00805f9b34fb")
-                    # serialService2 = beetle.services.get_service("0000dfb0-0000-1000-8000-00805f9b34fb")
-                    
-                    serialCharacteristic = serialService.get_characteristic("0000dfb1-0000-1000-8000-00805f9b34fb")
-                    for char in serialService.characteristics:
-                        print(char)
-                    # print(serialCharacteristic, serialCharacteristic.handle, serialCharacteristic.description)
-                    await beetle.start_notify(serialCharacteristic, notificationHandler)
-                    # await beetle.write_gatt_char(serialCharacteristic, bytes("A", "utf-8"))
-                    await asyncio.sleep(5.0)
-                    await beetle.stop_notify("0000dfb1-0000-1000-8000-00805f9b34fb")
-                    # comms = BLEComms(beetle, serialCharacteristic, index)
-                    # beetle.start_notify(serialCharacteristic, comms.notificationHandler)
+        if isFirstLoop:
+            serialSvc = beetle.getServiceByUUID(
+                "0000dfb0-0000-1000-8000-00805f9b34fb")
+            serialChar = serialSvc.getCharacteristics(
+                "0000dfb1-0000-1000-8000-00805f9b34fb")
+            delegate = Comms(serialChar, index)
+            beetle.withDelegate(delegate)
 
-
-                # print("Initialising handshake for Beetle", str(index))
-                # initHandshake(beetle, serialCharacteristic, index)
-
+        if not btleHandshakes[index]:
+            initHandshake(beetle, serialChar, index)
 
     except Exception as e:
-            print(e)
-
-
-# async def main(ble_address: str):
-#     device = await BleakScanner.find_device_by_address(ble_address, timeout=5.0)
-    # if not device:
-    #     raise BleakError(f"A device with address {ble_address} could not be found.")
-#     async with BleakClient(device) as client:
-#         svcs = await client.get_services()
-#         print("Services:")
-#         for service in svcs:
-#             print(service)
-
-
-if __name__ == "__main__":
-    # asyncio.run(beetleThread(sys.argv[1] if len(sys.argv) == 2 else beetleIDs[0]))
-    # asyncio.run(main(sys.argv[1] if len(sys.argv) == 2 else beetleIDs[0]))
-    asyncio.run(beetleThread(beetleIDs, 0))
+        print(e)
