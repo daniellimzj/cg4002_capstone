a43507f577e12692f9c454074cfddd7abaad2e50
daniellimzj <daniellimzijie@gmail.com>
2022-09-12 21:53:09 +0800

    Add ssh tunnel to input code

--- ext_comms/input.py
+++ ext_comms/input.py
@@ -1,19 +1,32 @@
-import os
-import sys
+import json
 from socket import *
+
+import sshtunnel
 
 if __name__ == "__main__":
 
-    serverName = 'localhost'
-    serverPort = 9696
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    print("connected to:", serverName, serverPort)
+    config = json.load(open('config.json'))
+    sunfirePassword = config["sunfirePassword"]
 
-    try:
-        while True:
-            message = input("Enter players' actions: ")
-            clientSocket.send(message.encode())
+    with sshtunnel.open_tunnel(
+        'stu.comp.nus.edu.sg',
+        ssh_username="danielim",
+        ssh_password=sunfirePassword,
+        remote_bind_address=('192.168.95.234', 9696),
+        local_bind_address=('localhost', 9696)
+    ) as tunnel:
 
-    finally:
-        clientSocket.close()
+            serverName = 'localhost'
+            serverPort = int(tunnel.local_bind_port)
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((serverName, serverPort))
+            print("connected to:", serverName, serverPort)
+
+            try:
+                while True:
+                    message = input("Enter players' actions: ")
+                    clientSocket.send(message.encode())
+
+            finally:
+                clientSocket.close()
+
