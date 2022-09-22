f4da4e6fb019256436aba12392786a507189d762
daniellimzj <daniellimzijie@gmail.com>
2022-09-22 16:42:57 +0800

    Fix (maybe) mqtt publishing code

--- ext_comms/mqtt_client_pub.py
+++ ext_comms/mqtt_client_pub.py
@@ -5,25 +5,13 @@ import MQTT
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(MQTT.Topics.gameState)
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
 
 if __name__ == "__main__":
-    try:
-        client = mqtt.Client()
-        client.on_connect = on_connect
-        client.on_message = on_message
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(MQTT.Configs.broker, MQTT.Configs.portNum)
 
-        client.connect(MQTT.Configs.broker, MQTT.Configs.portNum, 60)
-
-        while True:
-            client.publish(MQTT.Topics.gameState, input("are players in same area?"))
-
-
-    finally:
-        client.disconnect()
+    while True:
+        toPub = input("are players in same area?")
+        ret = client.publish(MQTT.Topics.gameState, toPub)
