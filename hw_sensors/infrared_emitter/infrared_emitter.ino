aed98ecbbf4c0d3d679c42ef1216e9e9b493ba4e
Rebecca Chua <e0564171@u.nus.edu>
2022-10-04 14:06:55 +0800

    Update infrared gun to light LED and comment print statements

--- hw_sensors/infrared_emitter/infrared_emitter.ino
+++ hw_sensors/infrared_emitter/infrared_emitter.ino
@@ -3,6 +3,7 @@
 
 #define PUSHBUTTON_PIN 2
 #define IR_SEND_PIN 3
+#define LED_PIN 4
 
 //values to transmit to receiver
 #define SEND_ADDRESS 0xA906 
@@ -14,7 +15,7 @@
 Button myButton(PUSHBUTTON_PIN); //create button object
 
 void setup() {
-  Serial.begin(115200);
+//  Serial.begin(115200);
   myButton.begin(); // initialize the button object
   IrSender.begin(); // Start with IR_SEND_PIN as send pin and if NO_LED_FEEDBACK_CODE is NOT defined, enable feedback LED at default feedback LED pin
 
@@ -26,10 +27,15 @@ void loop() {
   // If button pressed, send the code
   myButton.read();
   if (myButton.wasPressed()) {
-    Serial.println("Sending data");
-    Serial.flush();
+//    Serial.println("Sending data");
+//    Serial.flush();
 
     IrSender.sendNEC(SEND_ADDRESS, SEND_COMMAND, SEND_NUMBER_OF_REPEATS);
+
+    // light LED
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
   }
 //  delay(250);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 }
