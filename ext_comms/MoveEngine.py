a2f528f44879bf550702260d03b480ca95a133ca
daniellimzj <daniellimzijie@gmail.com>
2022-10-06 15:23:24 +0800

    Make moveEngine call AI

--- ext_comms/MoveEngine.py
+++ ext_comms/MoveEngine.py
@@ -2,18 +2,25 @@ from Player import Actions
 
 import multiprocessing as mp
 import BeetleMain as beetles
+from cg4002_capstone.hw_ai.real_data.predictor.main import MoveClassifier
 
 def startMoveProcess(actionQueue: mp.Queue, beetleQueue: mp.Queue):
 
+    classifier = MoveClassifier()
+
     while True:
-        p1Move, p2Move, didP1GetShot, didP2GetShot = getMoves(beetleQueue)
+        p1Move, p2Move, didP1GetShot, didP2GetShot = getMoves(beetleQueue, classifier)
 
         if p1Move != Actions.no or p2Move != Actions.no:
             actionQueue.put((p1Move, p2Move, didP1GetShot, didP2GetShot), block=True)
 
-def getMoves(beetleQueue: mp.Queue):
+def getMoves(beetleQueue: mp.Queue, classifier: MoveClassifier):
     p1Move = Actions.no
     p2Move = Actions.no
+
+    p1WristData = [0.0 * 24]
+    p2WristData = [0.0 * 24]
+    
     didP1GetShot = False
     didP2GetShot = False
 
@@ -21,10 +28,10 @@ def getMoves(beetleQueue: mp.Queue):
     beetleID = packet[beetles.PACKET_TYPE]
 
     if beetleID == beetles.P1_WRIST:
-        p1Move = getMoveFromAI(packet)
+        p1Move = classifier.classifyMove(packet)
 
     elif beetleID == beetles.P2_WRIST:
-        p2Move = getMoveFromAI(packet)
+        p2Move = classifier.classifyMove(packet)
 
     elif beetleID == beetles.P1_GUN:
         p1Move = Actions.shoot
