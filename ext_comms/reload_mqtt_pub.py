b649aa419220f9e66802c6bfc55216fdcf0ea18c
daniellimzj <daniellimzijie@gmail.com>
2022-10-07 13:31:51 +0800

    Provide mqtt test files for reload functionality

---
+++ ext_comms/reload_mqtt_pub.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+
+import MQTT
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(MQTT.Configs.broker, MQTT.Configs.portNum)
+
+    while True:
+        playerNum = input("Player that successfully reloaded: ")
+
+        if playerNum == "1":
+            client.publish(MQTT.Topics.didP1Reload, "1")
+
+        else:
+            client.publish(MQTT.Topics.didP2Reload, "1")
