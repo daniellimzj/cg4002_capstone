4e49a7f645f558292fd8cc933439b0d7eb5ee30c
BryanElmer <bryan.elmer.mulijono@gmail.com>
2022-10-11 00:21:44 +0800

    Add final beetle arm file

--- int_comms/beetleGun/infraredEmmiter.h
+++ int_comms/beetleGun/infraredEmmiter.h
@@ -5,8 +5,8 @@
 #define IR_SEND_PIN 3
 #define LED_PIN 4
 
-//values to transmit to receiver
-#define SEND_ADDRESS 0xA906 
+// values to transmit to receiver
+#define SEND_ADDRESS 0xA906
 #define SEND_COMMAND 0x0E
 #define SEND_NUMBER_OF_REPEATS 0
 
@@ -14,29 +14,35 @@
 
 #define MAX_BULLETS 6
 
-Button myButton(PUSHBUTTON_PIN); //create button object
+Button myButton(PUSHBUTTON_PIN); // create button object
 
 uint8_t numberOfBullets = MAX_BULLETS;
 
-void initEmmiter() {
+void initEmmiter()
+{
   myButton.begin(); // initialize the button object
   IrSender.begin(); // Start with IR_SEND_PIN as send pin and if NO_LED_FEEDBACK_CODE is NOT defined, enable feedback LED at default feedback LED pin
 }
 
-boolean senseEmmiter() {
+boolean senseEmmiter()
+{
   boolean isDetected = false;
   myButton.read();
-  if (myButton.wasPressed()) {
+  if (myButton.wasPressed())
+  {
     isDetected = true;
     // Only send IR signal if player has bullets
-    if (numberOfBullets > 0) {
+    if (numberOfBullets > 0)
+    {
       IrSender.sendNEC(SEND_ADDRESS, SEND_COMMAND, SEND_NUMBER_OF_REPEATS);
       numberOfBullets--;
       // light LED
       digitalWrite(LED_PIN, HIGH);
       delay(100);
       digitalWrite(LED_PIN, LOW);
-    } else {
+    }
+    else
+    {
       // flash LED
       digitalWrite(LED_PIN, HIGH);
       delay(100);
@@ -50,6 +56,7 @@ boolean senseEmmiter() {
   return isDetected;
 }
 
-void reloadGun() {
+void reloadGun()
+{
   numberOfBullets = MAX_BULLETS;
 }
