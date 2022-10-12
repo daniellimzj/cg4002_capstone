da9884334810b29b7a5ff73e04e2ce8d608e133d
BryanElmer <bryan.elmer.mulijono@gmail.com>
2022-10-12 15:45:51 +0800

    Add final files

--- int_comms/beetleVest/receiver.h
+++ int_comms/beetleVest/receiver.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
-#define PLAYER_NUM 1   // change depending on whether gun is for player 1 or 2
+#define PLAYER_NUM 1   // change depending on whether vest is for player 1 or 2
+// 1 means that this beetle will be on the vest of player 1
 
 #define IR_RECEIVE_PIN 2
 #define BUZZER_PIN 3
@@ -25,8 +26,8 @@ boolean senseReceiver() {
     boolean isDetected = false;
     if (IrReceiver.decode()) {
         if (IrReceiver.decodedIRData.command == RECEIVE_COMMAND) {
-            if ((PLAYER_NUM == 1 && IrReceiver.decodedIRData.address == PLAYER_1_SEND_ADDRESS) || 
-            (PLAYER_NUM == 2 && IrReceiver.decodedIRData.address == PLAYER_2_SEND_ADDRESS)) {
+            if ((PLAYER_NUM == 1 && IrReceiver.decodedIRData.address == PLAYER_2_SEND_ADDRESS) || 
+            (PLAYER_NUM == 2 && IrReceiver.decodedIRData.address == PLAYER_1_SEND_ADDRESS)) {
               isDetected = true;
               //flash the LED & sound the buzzer
               digitalWrite(INDICATOR_LED_PIN, HIGH);
