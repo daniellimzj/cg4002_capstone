83760232b5109f8136532d8561e9a979854370a6
daniellimzj <daniellimzijie@gmail.com>
2022-10-27 23:23:43 +0800

    Rename p1 files

---
+++ ext_comms/collect_run_daniel_bryan/test.py
@@ -0,0 +1,23 @@
+import os
+
+for i in range(1, 25):
+
+    oldfile = "p2_wrist_" + f'{i:04}' + ".txt"
+
+    action = ""
+
+    if 0 < i and i < 7:
+        action = "grenade"
+
+    elif 6 < i and i < 13:
+        action = "shield"
+
+    elif 12 < i and i < 19:
+        action = "reload"
+
+    else:
+        action = "logout"
+
+    newname = "p2_" + action + f'{i:04}' + ".txt"
+
+    os.rename(oldfile, newname)
