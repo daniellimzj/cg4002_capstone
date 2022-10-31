6b6aad9c2ff47f7744cd9ad40010693b08084d9c
daniellimzj <daniellimzijie@gmail.com>
2022-10-31 20:16:47 +0800

    Reshape array before transformation

--- ext_comms/MoveEngine.py
+++ ext_comms/MoveEngine.py
@@ -52,7 +52,7 @@ class MoveClassifier:
 
         testData = [5387.923076923077,12007,-7691,32407388.737672586,7462,2745.5,9614.0,-9195.692307692309,-4802,-14923,11716324.520710059,-8048,-12456.0,-6344.5,-1085.4102564102564,5792,-13853,46926777.318869166,3200,-7126.5,4584.0,-11869.846153846154,-3129,-23751,37783929.77120315,-9640,-16942.5,-7670.0,-1844.051282051282,4205,-11429,16273600.151216306,-1659,-4385.5,968.5,1678.3589743589744,17484,-19211,144046643.15318868,5518,-10452.0,10139.0]
 
-        testDataScaled = self.scaler.transform(testData)
+        testDataScaled = self.scaler.transform(np.array(testData).reshape(1, -1))
 
         print(testDataScaled)
 
@@ -60,7 +60,7 @@ class MoveClassifier:
     # data here is already processed
     def classifyMove(self, data):
 
-        scaled_data = self.scaler.transform(data)
+        scaled_data = self.scaler.transform(np.array(data).reshape(1, -1))
 
         self.input_buffer[:] = scaled_data
 
