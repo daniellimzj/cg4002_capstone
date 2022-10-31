fe25dbca5c87275ed2f6dfe2cbc63bf84353502b
See Yong Han <e0406263@u.nus.edu>
2022-10-31 16:35:58 +0800

    update model with kfolds cv

--- hw_ai/real_data_week10/mlp_sklearn_trial.py
+++ hw_ai/real_data_week10/mlp_sklearn_trial.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from hyperopt import fmin, tpe, Trials, hp, STATUS_OK
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
@@ -24,7 +24,7 @@ x = data[['meanaccX','maxaccX','minaccX','varaccX','medianaccX','25thpaccX','75t
           'meangyroZ','maxgyroZ','mingyroZ','vargyroZ','mediangyroZ','25thpgyroZ','75thpgyroZ']]
 
 y = data[['label']]
-scaler.fit_transform(x)
+x_scaled = scaler.fit_transform(x)
 
 # This is the testbench
 # x_validate = validate_data[['Mean-accX','Range-accX','Var-accX', 'Median-accX',
@@ -38,21 +38,16 @@ scaler.fit_transform(x)
 y = y.values.reshape(-1, )
 # y_validate = y_validate.values.reshape(-1, )
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.3, random_state=42)
 print("train size:" + str(len(x_train)))
 print("test size:" + str(len(x_test)))
-baseline_classifier = MLPClassifier(hidden_layer_sizes=(100,100),
-                                    activation='relu',)
+baseline_classifier = MLPClassifier()
 baseline_classifier.fit(x_train, y_train)
-y_baseline_pred = baseline_classifier.predict(x_test)
-# y_validate_pred = baseline_classifier.predict(x_validate)
-# print(y_validate_pred)
-# print("accuracy:" + str(accuracy_score(y_validate, y_validate_pred)))
-print("baseline accuracy:" + str(accuracy_score(y_test, y_baseline_pred)))
+print("baseline accuracy: %f" % baseline_classifier.score(x_test, y_test))
 #
 # # Hyperparameter Optimization
 coarse_search_space = \
-    {'hidden_layer_size': hp.choice('hidden_layer_sizes', range(1, 256)),
+    {'hidden_layer_size': hp.choice('hidden_layer_sizes', range(1, 1024)),
      'alpha': hp.lognormal('alpha', mu=np.log(1e-4), sigma=1),
      'solver': hp.choice('algorithm', ['sgd', 'adam']),
      'activation': hp.choice('activation', ['relu']),
@@ -67,12 +62,11 @@ def objective_fn(args):
         'activation': args['activation'],
         'solver': args['solver'],
         'learning_rate_init': args['learning_rate_init'],
-        'max_iter': 5000,
+        'max_iter': 3000,
     }
     classifier = MLPClassifier(**dict)
-    classifier.fit(x_train, y_train)
-    loss = -classifier.score(x_test, y_test)
-    return {'loss': loss, 'status': STATUS_OK, 'model': classifier}
+    loss = cross_val_score(classifier, x_train, y_train, cv=10, n_jobs=-1).mean()
+    return {'loss': -loss, 'status': STATUS_OK, 'model': classifier}
 
 
 trials = Trials()
@@ -80,10 +74,14 @@ best = fmin(objective_fn,
             space=coarse_search_space,
             algo=tpe.suggest,
             trials=trials,
-            max_evals=1000)
+            max_evals=30)
+solver = {0: 'sgd', 1: 'adam'}
+best_model = MLPClassifier(hidden_layer_sizes=best['hidden_layer_sizes'],
+                           alpha=best['alpha'],
+                           activation='relu',
+                           solver=solver[best['algorithm']],
+                           learning_rate_init=best['learning_rate_init']).fit(x_train, y_train)
 
-best_model = trials.best_trial['result']['model']
-plt.plot(best_model.loss_curve_)
 print("Training Accuracy: %f" % best_model.score(x_train, y_train))
 print("Testing Accuracy: %f" % best_model.score(x_test, y_test))
 y_pred = best_model.predict(x_test)
@@ -109,3 +107,4 @@ for i in range(len(best_model.intercepts_)):
     bias_str = bias_str.replace("]", "}")
     bias_file.write(bias_str)
     bias_file.close()
+
